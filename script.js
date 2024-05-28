a1f3ac5b3ad49ca84b11af864784b71694736f55
miguelolegna <miguel.olegna1@gmail.com>
2024-05-28 16:59:38 +0100

    pagina inicial + python.html(v1)

---
+++ script.js
@@ -0,0 +1,24 @@
+// Seleciona a imagem pelo ID
+var imagem = document.getElementById("Python");
+
+// Adiciona um evento de clique à imagem
+imagem.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "pyhon.html";
+});
+
+// Seleciona a imagem pelo ID
+var imagem2 = document.getElementById("UMLimage");
+// Adiciona um evento de clique à segunda imagem
+imagem2.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "Powerpoint.html";
+});
+
+// Seleciona a imagem pelo ID
+var imagem3 = document.getElementById("UMLimage");
+// Adiciona um evento de clique à segunda imagem
+imagem3.addEventListener("click", function() {
+    // Redireciona para o destino desejado
+    window.location.href = "UML.html";
+});
